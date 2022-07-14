5338b4e5b208d2e5cecc830b158fbd3eacd885ef
Jacob Johnston <jjoh661@lsu.edu>
2022-07-14 13:21:48 -0500

    create tournament_winner.py

---
+++ tournament_winner.py
@@ -0,0 +1,35 @@
+HOME_TEAM_WON = 1
+
+# O(n) time | O(k) space - where n is the number
+# of competitions and k is the number of teams
+def tournamentWinner(competitions, results):
+    currentBestTeam = ""
+    scores = {currentBestTeam: 0}
+
+    for idx, competition in enumerate(competitions):
+        result = results[idx]
+        homeTeam, awayTeam = competition
+
+        winningTeam = homeTeam if result == HOME_TEAM_WON else awayTeam
+
+        updateScores(winningTeam, 3, scores)
+
+        if scores[winningTeam] > scores[currentBestTeam]:
+            currentBestTeam = winningTeam
+
+    return currentBestTeam
+
+def updateScores(team, points, scores):
+    if team not in scores:
+        scores[team] = 0
+
+    scores[team] += points
+
+competitions1 = [
+    ["HTML", "C#"],
+    ["C#", "Python"],
+    ["Python", "HTML"],
+]
+results1 = [0, 0, 1]
+
+print(tournamentWinner(competitions1, results1))
